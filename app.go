74cada5b3e4b128dd9abaa0e8346891f4c4c6ce7
yacth_Mon <yacthmon@gmail.com>
2023-11-22 00:29:40 +0700

    feat: introduce godotenv to service

--- app.go
+++ app.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -18,6 +19,11 @@ var (
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+			log.Fatal("Error loading .env file")
+	}
+	
 	// Parse command-line flags
 	flag.Parse()
 
