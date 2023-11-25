8da65c1840dcbca30a333fa61728bc87cd22fca0
yacth_Mon <yacthmon@gmail.com>
2023-11-25 13:18:18 +0700

    fix: make id reveal in transaction

--- models/transaction.go
+++ models/transaction.go
@@ -5,10 +5,11 @@ import (
 )
 
 type Transaction struct {
-	AccountID     string `json:"accountId"`
-	TransactionID  string `json:"transactionId"`
-  Total float32 `json:"total"`//10000
-  TransactionDate time.Time `json:"transactionDate"`
-  TransactionLabel string `json:"transactionLabel"`
-  AccountType AccountType `json:"accountType"`
+  ID            string `bson:"_id"`
+	AccountID     string `bson:"accountId"`
+	TransactionID  string `bson:"transactionId"`
+  Total float32 `bson:"total"`//10000
+  TransactionDate time.Time `bson:"transactionDate"`
+  TransactionLabel string `bson:"transactionLabel"`
+  AccountType AccountType `bson:"accountType"`
 }
